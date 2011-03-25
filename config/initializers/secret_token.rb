8b5d037cad486eeba860bbec5da95dd826f54040
ashutosh kumar <ashutosh.k24@gmail.com>
2011-03-25 22:09:54 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ashu::Application.config.secret_token = '23934c884fefa2e3c32abc7ad4a9dbd05f499ff350043a6ea717bdaffde47a397718ef20ed57138c5316d1f01caf3d7d0c2e35e32d93c6fb5103bfb17cac9c7a'
